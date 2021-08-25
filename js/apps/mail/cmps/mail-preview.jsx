a69ecdb664754bd1a5e825106d4df6cd7685cb8b
ShirLaV <shir0lavi@gmail.com>
2021-08-25 18:55:44 +0300

    filter mail start

--- js/apps/mail/cmps/mail-preview.jsx
+++ js/apps/mail/cmps/mail-preview.jsx
@@ -8,7 +8,9 @@ function getTimeStr(time) {
     if (diff < 1000 * 60 * 60 * 24 * 2) return 'Yesterday';
 
     const date = new Date(time);
-    return date.toLocaleString()
+    const day = date.getDay();
+    const month = date.toLocaleString('en', { month: 'short' });
+    return month + ' ' + day
 }
 
 export function MailPreview({ mail }) {
@@ -16,8 +18,9 @@ export function MailPreview({ mail }) {
     return (
         <section className="flex mail-preview">
             <p>{mail.from}</p>
-            <p>{mail.subject}-</p>
-            <LongTxt text={mail.body} />
+            <p>{mail.subject} -</p>
+            {mail.body && <LongTxt text={mail.body} />}
+            {!mail.body && <p>No content</p>}
             <p>{getTimeStr(mail.sentAt)}</p>
         </section>
     )
