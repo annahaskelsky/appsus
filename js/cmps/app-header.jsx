0a0fba03b6d648d074d4de03bbd73e6034c52155
Anna Haskelsky <annahaskelsky@gmail.com>
2021-08-25 20:10:41 +0300

    more css tries

--- js/cmps/app-header.jsx
+++ js/cmps/app-header.jsx
@@ -5,7 +5,7 @@ const _AppHeader = (props) => {
     return (
         <section className="app-header flex space-between align-center main-layout">
             <div className="logo-container flex align-center" onClick={() => props.history.push('/')}>
-                <img src="../../assets/img/logo.png" />
+                <img src="../assets/img/logo.png" />
                 <h1 >Appsus</h1>
             </div>
             <nav className="flex">
