453f8c5268efb11e9bfe454e54442e11b0520081
Anna Haskelsky <annahaskelsky@gmail.com>
2021-08-26 11:26:37 +0300

    started notes filter

--- js/apps/keep/cmps/note-filter.jsx
+++ js/apps/keep/cmps/note-filter.jsx
@@ -8,16 +8,32 @@ export class NoteFilter extends React.Component {
     }
 
     handleChange = (ev) => {
-        const value = ev.target.value
-        this.setState({ filterBy: { ...this.state.filterBy, txt: value } }, () => {
-          this.props.onSetFilter(this.state.filterBy)  
-        })
+        console.log(ev.type);
+        if(ev.type === 'click') {
+            console.log(ev);
+            this.setState({ filterBy: { ...this.state.filterBy, type: ev.target.value } }, () => {
+                // this.props.onSetFilter(this.state.filterBy)
+            })
+        } else {
+            const value = ev.target.value
+            this.setState({ filterBy: { ...this.state.filterBy, txt: value } }, () => {
+                // this.props.onSetFilter(this.state.filterBy)
+            })
+        }
+        console.log(this.state);
     }
 
     render() {
-        const {txt, type} = this.state.filterBy
+        const { txt, type } = this.state.filterBy
         return (
-            <input type="text" name="txt-filter" placeholder="Search for a note" value={txt} onChange={this.handleChange} />
+            <section className="note-filter">
+                <input type="text" name="txt-filter" placeholder="Search for a note" value={txt} onChange={this.handleChange} />
+                <div className="filter-by-type">
+                    <div onClick={() => this.handleChange('image')} name="image"><i className="far fa-image" ></i></div>
+                    <div onClick={this.handleChange} value="todo"><i className="fas fa-list-ul"></i></div>
+                    <div onClick={this.handleChange} value="text"><i className="fas fa-file-alt"></i></div>
+                </div>
+            </section>
         )
     }
 }
