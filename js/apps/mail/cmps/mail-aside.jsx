a707b0e221a0623a116f26f17b4c54fae7fc5d71
ShirLaV <shir0lavi@gmail.com>
2021-08-27 21:24:29 +0300

    toggle aside mail app

--- js/apps/mail/cmps/mail-aside.jsx
+++ js/apps/mail/cmps/mail-aside.jsx
@@ -22,32 +22,32 @@ export class MailAside extends React.Component {
         const { unReadMails } = this.state
         return (
             <section className="mail-aside-container">
-                <Link to="/mail/new" className="compose-btn"><i className="fas fa-plus"></i> Compose</Link>
+                <Link to="/mail/new" className="compose-btn"><i className="fas fa-plus"></i> <span>Compose</span></Link>
                 <ul className="clean-list  flex">
                     <li>
                         <NavLink to="/mail/inbox" className="flex align-center">
-                            <i className="fas fa-envelope"></i> Inbox
-                            <span>{unReadMails}</span>
+                            <i className="fas fa-envelope"></i><span>Inbox</span>
+                            <span>({unReadMails})</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/mail/starred" className="flex align-center">
-                            <i className="fas fa-star"></i> Starred
+                            <i className="fas fa-star"></i><span>Starred</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/mail/sent" className="flex align-center">
-                            <i className="fas fa-paper-plane"></i> Sent
+                            <i className="fas fa-paper-plane"></i><span>Sent</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/mail/draft" className="flex align-center">
-                            <i className="fas fa-file"></i> Draft
+                            <i className="fas fa-file"></i><span>Draft</span>
                         </NavLink>
                     </li>
                     <li>
                         <NavLink to="/mail/trash" className="flex align-center">
-                            <i className="fa fa-trash" aria-hidden="true"></i> Trash
+                            <i className="fa fa-trash" aria-hidden="true"></i><span>Trash</span>
                         </NavLink>
                     </li>
                 </ul>
--- js/apps/mail/mail-index.jsx
+++ js/apps/mail/mail-index.jsx
@@ -6,23 +6,37 @@ import { MailDetails } from './pages/mail-details.jsx';
 import { MailList } from './pages/mail-list.jsx';
 import { MailAdd } from './pages/mail-add.jsx';
 
-export function MailApp() {
-    return (
-        <section className="mail-app flex main-layout">
-            <aside>
-                <MailAside />
-            </aside>
-            <main>
-                <MailFilter />
-                <Switch>
-                    <Route path="/mail/edit/:mailId" component={MailAdd}/>
-                    <Route path="/mail/new" component={MailAdd}/>
-                    <Route path="/mail/:mailFilter/:mailId" component={MailDetails}/>
-                    <Route path="/mail/:mailFilter" component={MailList} />
-                    <Route path="/mail" component={MailList} />
-                </Switch>
-            </main>
-        </section>
+export class MailApp extends React.Component {
 
-    )
+    state = {
+        isSideMenuOpen: false
+    }
+
+    toggleSideMemu = () => {
+        console.log('toggling')
+        this.setState({ isSideMenuOpen: !this.state.isSideMenuOpen })
+    }
+
+    render() {
+        const { isSideMenuOpen } = this.state;
+
+        return (
+            <section className="mail-app flex main-layout">
+                {/* <aside className="aside-menu-open"> */}
+                <aside className={isSideMenuOpen? 'aside-menu-open' : ''}>
+                    <MailAside />
+                </aside>
+                <main>
+                    <MailFilter toggleSideMemu={this.toggleSideMemu} />
+                    <Switch>
+                        <Route path="/mail/edit/:mailId" component={MailAdd} />
+                        <Route path="/mail/new" component={MailAdd} />
+                        <Route path="/mail/:mailFilter/:mailId" component={MailDetails} />
+                        <Route path="/mail/:mailFilter" component={MailList} />
+                        <Route path="/mail" component={MailList} />
+                    </Switch>
+                </main>
+            </section>
+        )
+    }
 }
