3eedb3ceaaa1740203f3bbb7e920bc8deee85c37
ShirLaV <shir0lavi@gmail.com>
2021-08-27 12:28:42 +0300

    home text change

--- js/pages/app-home.jsx
+++ js/pages/app-home.jsx
@@ -9,31 +9,26 @@ export function Home() {
                 <nav className="main-nav flex">
                     <Link to="/mail"><div className="main-layout flex">
                         <img src="./assets/img/email.png" />
-                        <article>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                            Sed aspernatur error perspiciatis, velit aliquid voluptate laudantium,
-                            consequuntur voluptatem doloremque odio debitis aperiam voluptates?
-                            Aliquam reprehenderit eius possimus voluptatibus necessitatibus reiciendis
-                            ipsa facilis, ullam, eaque iste, enim doloremque? Ea, placeat illo,
-                            ipsam quidem autem, vel omnis eaque id tempore architecto ullam!</article>
+                        <article>Easy to use email app that saves you time and keeps your messages safe.
+                            Stay in touch with your friends an coworkers, in an intuitive, efficient, and useful way.
+                        </article>
                     </div></Link>
                     <Link to="/keep">
-                    <div className="main-layout flex">
-                        <img src="./assets/img/notes.jpeg" />
-                        <article>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                            Sed aspernatur error perspiciatis, velit aliquid voluptate laudantium,
-                            consequuntur voluptatem doloremque odio debitis aperiam voluptates?
-                            Aliquam reprehenderit eius possimus voluptatibus necessitatibus reiciendis
-                            ipsa facilis, ullam, eaque iste, enim doloremque? Ea, placeat illo,
-                            ipsam quidem autem, vel omnis eaque id tempore architecto ullam!</article>
-                    </div></Link>
+                        <div className="main-layout flex">
+                            <img src="./assets/img/notes.jpeg" />
+                            <article>Notes app that makes it easy to capture a thought or list for yourself,
+                                and share it with friends and family.
+                                Quickly capture what’s on your mind and get a reminder later at the right place or time.
+                                Grab a photo of a poster, receipt or document and easily organize or find it later in search.
+                            </article>
+                        </div></Link>
                     <Link to="/book"><div className="main-layout flex">
                         <img src="./assets/img/books.png" />
-                        <article>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                            Sed aspernatur error perspiciatis, velit aliquid voluptate laudantium,
-                            consequuntur voluptatem doloremque odio debitis aperiam voluptates?
-                            Aliquam reprehenderit eius possimus voluptatibus necessitatibus reiciendis
-                            ipsa facilis, ullam, eaque iste, enim doloremque? Ea, placeat illo,
-                            ipsam quidem autem, vel omnis eaque id tempore architecto ullam!</article>
+                        <article>Here you will find contemporary bestsellers, new book editions, book promotions and much more.
+                            We have popular genres like literature and fiction, children's books, suspense and mystery, cooking,
+                            comics and graphic novels, novels, science fiction and fantasy. 
+                            In addition, you will find great recommendations for books that may interest you based on your search.>
+                        </article>
                     </div></Link>
                 </nav>
             </main>
