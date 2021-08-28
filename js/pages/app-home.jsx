f5bdae83a92ae0db837822967adaf456c14f564d
Reem Ben-David <reemile1996@gmail.com>
2021-08-28 14:12:26 +0300

    refactored homepage

--- js/pages/app-home.jsx
+++ js/pages/app-home.jsx
@@ -1,40 +1,101 @@
-const { Link } = ReactRouterDOM
-
+const { Link } = ReactRouterDOM;
 
 export function Home() {
-    return (
-        <section className="home">
-            <div className="hero-container">
-                <h1 className="main-layout">Hello Appsus</h1>
+  return (
+    <section className='home'>
+      <section className='main-hero-container app-hero-section'>
+        <h1>Welcome to Appsus</h1>
+      </section>
+      <section className='main-description description-section'>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ipsum
+          porro dolorum officia maiores et asperiores nesciunt reiciendis
+          debitis. Est.
+        </p>
+      </section>
+      <section className='app-hero-section notes-hero'>
+        <h1>
+          <Link to='/keep'>Miss Keep</Link>
+        </h1>
+      </section>
+      <section className='description-section'>
+        {/* <img src='./assets/img/note.png' /> */}
+        <p className="main-layout">
+          Notes app that makes it easy to capture a thought or list for
+          yourself, and share it with friends and family. Quickly capture what’s
+          on your mind and get a reminder later at the right place or time. Grab
+          a photo of a poster, receipt or document and easily organize or find
+          it later in search.
+        </p>
+      </section>
+      <section className='app-hero-section email-hero'>
+        <h1>
+          <Link to='/mail'>Mr Email</Link>
+        </h1>
+      </section>
+      <section className='description-section'>
+        {/* <img src='./assets/img/email.png' /> */}
+        <p className="main-layout">
+          Easy to use email app that saves you time and keeps your messages
+          safe. Stay in touch with your friends an coworkers, in an intuitive,
+          efficient, and useful way.
+        </p>
+      </section>
+      <section className='app-hero-section book-hero'>
+        <h1>
+          <Link to='/book'>Miss Book</Link>
+        </h1>
+      </section>
+      <section className='description-section'>
+        {/* <img src='./assets/img/email.png' /> */}
+        <p className="main-layout">
+          Here you will find contemporary bestsellers, new book editions, book
+          promotions and much more. We have popular genres like literature and
+          fiction, children's books, suspense and mystery, cooking, comics and
+          graphic novels, novels, science fiction and fantasy. In addition, you
+          will find great recommendations for books that may interest you based
+          on your search.
+        </p>
+      </section>
+      {/* <main>
+        <nav className='main-nav flex'>
+          <Link to='/mail'>
+            <div className='main-layout flex'>
+              <img src='./assets/img/email.png' />
+              <article>
+                Easy to use email app that saves you time and keeps your
+                messages safe. Stay in touch with your friends an coworkers, in
+                an intuitive, efficient, and useful way.
+              </article>
             </div>
-            <main>
-                <nav className="main-nav flex">
-                    <Link to="/mail"><div className="main-layout flex">
-                        <img src="./assets/img/email.png" />
-                        <article>Easy to use email app that saves you time and keeps your messages safe.
-                            Stay in touch with your friends an coworkers, in an intuitive, efficient, and useful way.
-                        </article>
-                    </div></Link>
-                    <Link to="/keep">
-                        <div className="main-layout flex">
-                            <img src="./assets/img/note.png" />
-                            <article>Notes app that makes it easy to capture a thought or list for yourself,
-                                and share it with friends and family.
-                                Quickly capture what’s on your mind and get a reminder later at the right place or time.
-                                Grab a photo of a poster, receipt or document and easily organize or find it later in search.
-                            </article>
-                        </div></Link>
-                    <Link to="/book"><div className="main-layout flex">
-                        <img src="./assets/img/books.png" />
-                        <article>Here you will find contemporary bestsellers, new book editions, book promotions and much more.
-                            We have popular genres like literature and fiction, children's books, suspense and mystery, cooking,
-                            comics and graphic novels, novels, science fiction and fantasy. 
-                            In addition, you will find great recommendations for books that may interest you based on your search.>
-                        </article>
-                    </div></Link>
-                </nav>
-            </main>
-        </section>
-
-    )
+          </Link>
+          <Link to='/keep'>
+            <div className='main-layout flex'>
+              <img src='./assets/img/note.png' />
+              <article>
+                Notes app that makes it easy to capture a thought or list for
+                yourself, and share it with friends and family. Quickly capture
+                what’s on your mind and get a reminder later at the right place
+                or time. Grab a photo of a poster, receipt or document and
+                easily organize or find it later in search.
+              </article>
+            </div>
+          </Link>
+          <Link to='/book'>
+            <div className='main-layout flex'>
+              <img src='./assets/img/books.png' />
+              <article>
+                Here you will find contemporary bestsellers, new book editions,
+                book promotions and much more. We have popular genres like
+                literature and fiction, children's books, suspense and mystery,
+                cooking, comics and graphic novels, novels, science fiction and
+                fantasy. In addition, you will find great recommendations for
+                books that may interest you based on your search.
+              </article>
+            </div>
+          </Link>
+        </nav>
+      </main> */}
+    </section>
+  );
 }
