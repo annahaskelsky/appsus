cde13550689f47def84bac2b3f47be4a524d8f5c
Anna Haskelsky <annahaskelsky@gmail.com>
2021-08-28 10:26:25 +0300

    about page css + header nav

--- js/pages/app-about.jsx
+++ js/pages/app-about.jsx
@@ -6,26 +6,26 @@ export function About() {
 
       <h1>About Us</h1>
       <section className="team flex">
-        <div>
+        <div className="team-member">
           <img src="./assets/img/shir.jpg" />
           <h2>Shir Lavi</h2>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt molestiae qui aut, eveniet necessitatibus, animi hic repellendus quia doloremque sint, dolorem alias voluptatum incidunt illo aperiam consequuntur sit adipisci repudiandae! Delectus totam sed voluptate ullam tempore hic quod similique error minus, non iusto, autem possimus dolorem quasi. Officia, sequi a.
           </p>
           <div className="flex socials">
-            <i className="fab fa-facebook-square"></i>
-            <i className="fab fa-github-square"></i>
-            <i className="fab fa-linkedin"></i>
+            <a href="https://www.facebook.com/shir.lavi.5682" target="_blank"><i className="fab fa-facebook-square"></i></a>
+            <a href="https://github.com/ShirLaV" target="_blank"><i className="fab fa-github-square"></i></a>
+            <a href="https://www.linkedin.com/in/shir-lavi-621957218/" target="_blank"><i className="fab fa-linkedin"></i></a>
           </div>
         </div>
-        <div>
+        <div className="team-member">
           <img src="./assets/img/anna.jpg" />
           <h2>Anna Haskelsky</h2>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt molestiae qui aut, eveniet necessitatibus, animi hic repellendus quia doloremque sint, dolorem alias voluptatum incidunt illo aperiam consequuntur sit adipisci repudiandae! Delectus totam sed voluptate ullam tempore hic quod similique error minus, non iusto, autem possimus dolorem quasi. Officia, sequi a.
           </p>
           <div className="flex socials">
-            <i className="fab fa-facebook-square"></i>
-            <i className="fab fa-github-square"></i>
-            <i className="fab fa-linkedin"></i>
+            <a href="https://www.facebook.com/anna.haskelsky/" target="_blank"><i className="fab fa-facebook-square"></i></a>
+            <a href="https://github.com/annahaskelsky" target="_blank"><i className="fab fa-github-square"></i></a>
+            <a href="https://www.linkedin.com/in/anna-haskelsky-70a928216/" target="_blank"><i className="fab fa-linkedin"></i></a>
           </div>
         </div>
       </section>
